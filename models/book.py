610e7e09babc60d8d620a1bbfc3fd83f18827ffc
Kiwrght <kiwrght@uiowa.edu>
2025-04-10 11:37:16 -0500

    Updated attributes of the model classes
    added all of the attributes to the books and user classes and made some to be Optional so there isnt a null error

--- models/book.py
+++ models/book.py
@@ -1,12 +1,23 @@
+from typing import Optional
 from beanie import Document
+from datetime import datetime
+
+from todo import BookStatus
 
 
 class Book(Document):
-    name: str
+    title: str
     author: str
     genre: str
-    review: str
-    rating: float
+    book_status: BookStatus = BookStatus.to_read
+    rating: int
+    userId: str  # Reference to the user who added this book
+    isbn: Optional[str] = None
+    dateStarted: Optional[datetime] = None
+    dateCompleted: Optional[datetime] = None
+    review: Optional[str] = None
+    createdAt: datetime = datetime.now()
+    updatedAt: datetime = datetime.now()
 
     class Settings:
         name = "books"  # Specify the collection name for Book documents
--- models/user.py
+++ models/user.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+from typing import Optional  # allows a value to be None without it being an Null  error
 from beanie import Document
 
 
 class User(Document):
     username: str
+    email: str
     password: str
+    dateJoined: datetime = datetime.now()
+    lastLogin: Optional[datetime] = None
 
     class Settings:
         name = "users"  # Specify the collection name for User documents
