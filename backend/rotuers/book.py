76e45bdf846369fd3972d8ccb21f4c3920c07645
Kiwrght <kiwrght@uiowa.edu>
2025-04-16 23:07:31 -0500

    moved models folder made router files
    made router codes for user and book. user is complete

---
+++ backend/rotuers/book.py
@@ -0,0 +1,66 @@
+from fastapi import APIRouter, HTTPException, status, Path
+from models.book import Book, BookRequest
+
+book_router = APIRouter()
+
+books_list = []  # List to store books
+max_id: int = 0  # Variable to store the maximum ID of the book
+
+
+@book_router.post("", status_code=status.HTTP_201_CREATED)
+async def add_book(book: BookRequest) -> Book:
+    global max_id
+    max_id += 1
+    new_book = Book(
+        id=max_id,
+        title=book.title,
+        author=book.author,
+        genre=book.genre,
+        book_status=book.book_status,
+        rating=book.rating,
+        isbn=None,
+    )
+    await Book.insert_one(new_book)  # Save the book to the database
+    return new_book
+
+
+@book_router.get("")
+async def get_books() -> list[Book]:
+    return await Book.find_all().to_list()
+
+
+@book_router.get("/{book_id}")
+async def get_book_by_id(book_id: int = Path(..., title="default")) -> Book:
+    book = Book.get(book_id)
+    if book:
+        return book
+    else:  # If the book is not found, raise a 404 error
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Book with ID = {book_id} was not found",
+        )
+
+
+@book_router.put("/{book_id}")
+async def update_book(book_id: int, book: BookRequest) -> dict:
+    for x in books_list:
+        if x.id == book_id:
+            x.title = book.title
+            x.author = book.author
+            x.genre = book.genre
+            x.book_status = book.book_status
+            x.rating = book.rating
+            return {"message": "Book updated successfully"}
+
+    return {"message": f"The todo with ID={book_id} is not found."}
+
+
+@book_router.delete("/{book_id}")
+async def delete_book(book_id: int) -> dict:
+    for x in range(len(books_list)):
+        book = books_list[x]
+        if book.id == book_id:
+            books_list.pop(x)
+            return {"message": f"Book with ID={book_id} has deleted successfully"}
+
+    return {"message": f"The todo with ID={book_id} is not found."}
