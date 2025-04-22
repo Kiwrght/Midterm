823143c30cbf33d5447d103a839f4554ba465d91
Kiwrght <kiwrght@uiowa.edu>
2025-04-22 13:42:42 -0500

    update User_model.py
    added UserRequest for  signing up

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,14 +1,26 @@
 from datetime import datetime
 from typing import Optional  # allows a value to be None without it being an Null  error
 from beanie import Document
+from pydantic import BaseModel
 
 
 class User(Document):
     username: str
     email: str
     password: str
+    role: str = ""  # Default role for new users
     dateJoined: datetime = datetime.now()
     lastLogin: Optional[datetime] = None
 
     class Settings:
         name = "users"  # Specify the collection name for User documents
+
+
+class UserRequest(BaseModel):
+    """
+    # model for user sign up
+    """
+
+    username: str
+    email: str
+    password: str  # plain text from user input
